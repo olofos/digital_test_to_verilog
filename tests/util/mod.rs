0dd9f8d074e9b2ea3c1ce761d2e51a10676e8bda
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-05-18 23:43:39 +0200

    Only test utils once

--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -33,29 +33,3 @@ impl TempDir {
         let _ = std::fs::remove_dir_all(self.path);
     }
 }
-
-#[test]
-fn temp_dir_works() {
-    let dir = TempDir::create("abc");
-
-    let path = dir.path.clone();
-    println!("Created {}", path.to_string_lossy());
-
-    assert!(path
-        .components()
-        .last()
-        .unwrap()
-        .as_os_str()
-        .to_string_lossy()
-        .starts_with("abc_"));
-    assert!(path.exists());
-    assert!(path.is_dir());
-
-    let filename = dir.file("foobar123");
-    assert!(filename.to_string_lossy().contains("abc_"));
-    assert!(filename.to_string_lossy().contains("foobar123"));
-
-    dir.delete();
-
-    assert!(!path.exists());
-}
